338402d81b37d013f6ea8de31390613f1aaac5be
TurcanIvan <68420218+TurcanIvan@users.noreply.github.com>
2021-12-01 20:52:20 +0200

    HW

---
+++ 2d.while.patternHW.py
@@ -0,0 +1,54 @@
+
+#  HW: 5x5 DARW TRIS
+#      * 
+#      * * 
+#      * * * 
+#      * * * * 
+#      * * * * *
+
+print("\n")
+# ######## draw 1 stars #####
+s = 1 
+while s <= 1:
+    print("*",end=" ")
+    if s % 1 == 0:
+        print()
+    s += 1
+# ######## draw 2 stars #####
+
+# ######## draw 2 stars #####
+s = 1 
+while s <= 2:
+    print("*",end=" ")
+    if s % 2 == 0:
+        print()
+    s += 1
+# ######## draw 3 stars #####
+
+# ######## draw 3 stars #####
+s = 1 
+while s <= 3:
+    print("*",end=" ")
+    if s % 3 == 0:
+        print()
+    s += 1
+# ######## draw 4 stars #####
+
+# ######## draw 4 stars #####
+s = 1 
+while s <= 4:
+    print("*",end=" ")
+    if s % 4 == 0:
+        print()
+    s += 1
+# ######## draw 5 stars #####
+
+# ######## draw 5 stars #####
+s = 1 
+while s <= 5:
+    print("*",end=" ")
+    if s % 5 == 0:
+        print()
+    s += 1
+# ######## draw 1 stars #####
+print("\n")
